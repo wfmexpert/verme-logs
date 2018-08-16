22aa1b4a25471d8ace8a1d55a6d5b73a0b8d8587
tabakov <i.tabakov@netall.su>
2018-08-16 19:15:10 +0300

    update db router

--- applogs/db_router.py
+++ applogs/db_router.py
@@ -21,7 +21,5 @@ class LogsDBRouter():
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label == self.LOG_APP_NAME:
-            if db != self.LOG_APP_DB_ALIAS:
-                return None
-            return True
+            return db == self.LOG_APP_DB_ALIAS
         return None
