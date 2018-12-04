7b23ab51ce14b0a2e445aa7da119231f45a82152
Ilya Tabakov <i.tabakov@netall.su>
2018-12-04 18:11:50 +0300

    https://github.com/wfmexpert/verme-logs/issues/3

--- xlsexport/methods.py
+++ xlsexport/methods.py
@@ -5,10 +5,13 @@ def get_report_by_code(code=None):
     """
     Возвращает сформированный отчет по его коду
     """
+    print("CODE", code)
+
     if not code:
         return None
 
     template = ExportTemplate.objects.filter(code=code).first()
+    print("TEMPLATE", template)
     if not template:
         return None
 
