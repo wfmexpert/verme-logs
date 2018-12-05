91ee2895c3c6b95e85e6a0d7dd6ca2dc92fa7197
Ilya Tabakov <i.tabakov@netall.su>
2018-12-05 17:54:12 +0300

    https://github.com/wfmexpert/verme-logs/issues/3

--- xlsexport/models.py
+++ xlsexport/models.py
@@ -169,7 +169,8 @@ class ExportTemplate(models.Model):
                             attr_value = attr_value.strftime(field.get('format'))
                 if isinstance(attr_value, float):
                     if param_fields and field.get('format'):
-                        cell_format = {'num_format_str': field.get('format')}
+                        cell_format = xlwt.XFStyle()
+                        cell_format.num_format_str = field.get('format')
                 if not cell_format:
                     worksheet.write(row, idx, str(attr_value))
                 else:
