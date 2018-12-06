3ca603305b4d416d0b6912460f84a9fda35dccd5
Ilya Tabakov <i.tabakov@netall.su>
2018-12-06 18:20:24 +0300

    https://github.com/wfmexpert/verme-logs/issues/3 templates

--- xlsexport/admin.py
+++ xlsexport/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from .models import ExportTemplate
 from .forms import ExportTemplateForm
 
+from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
+
+
+@staff_member_required
+def xls_export_view(request):
+    if request.method == 'POST':
+        export_template = ExportTemplate.objects.filter(id=request.POST['export_template_id'])
+        if not export_template:
+            messages.error(request, "Шаблон отчета не найден.")
+        export_template.to_export()
+    redirect_url = request.META.get('HTTP_REFERER', reverse('admin:xlsexport_exporttemplate_changelist'))
+    return HttpResponseRedirect(redirect_url)
 
 @admin.register(ExportTemplate)
 class ExportTemplateAdmin(admin.ModelAdmin):
