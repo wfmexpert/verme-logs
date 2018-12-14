28ebb6125208149d9a6820d44d13fb6f2f5f8aac
tabakov <i.tabakov@netall.su>
2018-12-14 12:05:48 +0300

    https://github.com/wfmexpert/verme-logs/issues/7

---
+++ wfm_admin/admin.py
@@ -0,0 +1,82 @@
+from django.contrib.admin import *
+
+import logging
+
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+
+class WfmAdminSite(AdminSite):
+    sections = None
+
+    def __init__(self, *args, **kwargs):
+        super(WfmAdminSite, self).__init__(*args, **kwargs)
+        self.sections = settings.ADMIN_SECTIONS
+        self.columns = settings.ADMIN_COLUMNS
+        self.site_header = 'Настройки'
+        self.index_title = self.site_header
+        self._registry.update(site._registry)
+
+    def get_app_list(self, request):
+        """
+        Вот тут то мы и подменим логику "Приложение->модели" на "Секция->модели"
+        :param request:
+        :return:
+        """
+        sections_list = []
+        app_list = super(self.__class__, self).get_app_list(request)
+        self.current_user = request.user
+
+        if len(app_list) == 0:  # в тестах список пустой, пропускаем. TODO
+            return app_list
+
+        def get_model_from_app_list(app_name, obj_name):
+            try:
+                app = [app for app in app_list if app['app_label'] == app_name][0]
+            except IndexError:
+                return
+            try:
+                return [model for model in app['models'] if model['object_name'] == obj_name][0]
+            except IndexError:
+                return
+
+        def check_section_access(self, key):
+            for model in self.sections[key]:
+                splitted_value = model.lower().split('.', 1)
+                permission_string = f'{splitted_value[0]}.add_{splitted_value[1]}'
+                if self.current_user.has_perm(permission_string):
+                    return True
+            return False
+
+        def get_sections_by_user(self):
+            for key in sorted(self.sections.keys()):
+                section = {'has_module_perms': False, 'app_label': key, 'app_url': '', 'name': key, 'models': []}
+                if check_section_access(self, key):
+                    section.update({'has_module_perms': True})
+                for m_name in self.sections[key]:
+                    app_name, obj_name = m_name.split('.')
+                    model = get_model_from_app_list(app_name, obj_name)
+                    if model:
+                        section['models'].append(model)
+                if section.get('has_module_perms'):
+                    sections_list.append(section)
+
+        get_sections_by_user(self)
+
+        columns = {}
+        col = 0
+        for sects in self.columns:
+            for s in sects:
+                for section in sections_list:
+                    if section['app_label'] == s:
+                        try:
+                            columns[col].append(section)
+                        except KeyError:
+                            columns[col] = [section]
+            col += 1
+            # Так как сортировка по алфавиту нам не подходит
+        return columns
+
+wfm_admin = WfmAdminSite()
+
---
+++ wfm_admin/management/commands/generate_groups.py
@@ -0,0 +1,72 @@
+#
+# Copyright 2018 ООО «Верме»
+#
+# Файл создания групп и прав доступа для секций в административном разделе
+#
+
+# coding=utf-8;
+
+from django.apps import apps
+from django.contrib.auth.models import Group, Permission
+from django.core.management.base import BaseCommand
+from wfm.settings import ADMIN_SECTIONS
+
+
+class Command(BaseCommand):
+    help = 'Generate section groups and permissions'
+
+    def get_sections(self):
+        return ADMIN_SECTIONS
+
+    def get_groups(self):
+        return Group.objects.all()
+
+    def get_section_models(self, key):
+        sections = self.get_sections()
+        for m_name in sections[key]:
+            yield m_name
+
+    def get_related_models(self, model):
+        fields = apps.get_model(model)._meta.get_fields()
+        for field in fields:
+            if field.is_relation and field.related_model:
+                yield field.related_model
+
+    def get_related_perms(self, model):
+        related_models = self.get_related_models(model)
+        for rmodel in related_models:
+            splitted_name = rmodel._meta.label.lower().split('.', 1)
+            yield f"{splitted_name[0]}.change_{splitted_name[1]}"
+
+    def get_model_perms(self, model):
+        splitted_name = model.lower().split('.', 1)
+        return [
+            f"{splitted_name[0]}.add_{splitted_name[1]}",
+            f"{splitted_name[0]}.change_{splitted_name[1]}",
+            f"{splitted_name[0]}.delete_{splitted_name[1]}"]
+
+    def get_group_by_section(self, section):
+        combined_name = f"Управление {section.lower()}"
+        group, created = Group.objects.get_or_create(name=combined_name)
+        return group, created
+
+    def get_permissions_list(self, section_permissions):
+        perm_list = list()
+        for perm in section_permissions:
+            perm_obj = Permission.objects.filter(codename=perm.split('.')[1]).first()
+            if perm_obj:
+                perm_list.append(perm_obj)
+        return perm_list
+
+    def handle(self, *args, **options):
+        for section in self.get_sections():
+            section_perms = set()
+            for model in self.get_section_models(section):
+                model_perms = set()
+                model_perms.update(self.get_model_perms(model))
+                model_perms.update(list(self.get_related_perms(model)))
+                section_perms.update(model_perms)
+            group, created = self.get_group_by_section(section)
+            if not created:
+                group.permissions.clear()
+            group.permissions.set(self.get_permissions_list(section_perms))
