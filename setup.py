890362c682d81996248196115998adce9e51ee66
tabakov <i.tabakov@netall.su>
2018-12-18 16:24:10 +0300

    https://github.com/wfmexpert/verme-logs/issues/10

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
+        'Framework :: Django :: 2.0',  # replace "X.Y" as appropriate
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',  # example license
         'Operating System :: OS Independent',
