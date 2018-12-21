88c3bb0019eaf75ba795bfa68f86d5459ae469b6
tabakov <i.tabakov@netall.su>
2018-12-21 18:59:56 +0300

    fix add permission bug

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='verme-logs',
-    version='1.06',
+    version='1.07',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
