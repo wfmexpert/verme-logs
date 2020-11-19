72c795e9281f8460e7aa72ea98d69ef741fc2943
a.linev <a.linev@verme.ru>
2020-11-19 18:23:51 +0300

    6493 конвертация в JSON для поля headers

--- applogs/views.py
+++ applogs/views.py
@@ -1,6 +1,9 @@
-from .models import ClientRecord
+import json
+
 from django.http import HttpResponse
 from django.views.generic import View
+
+from .models import ClientRecord
 
 
 class CreateView(View):
@@ -8,8 +11,12 @@ class CreateView(View):
     def post(self, request):
         message = request.body.decode('utf-8')
         user_agent = request.META['HTTP_USER_AGENT']
+        username = str(request.user)
+        headers = None
+        if request.headers:
+            headers = json.dumps(dict(request.headers))
 
-        record = ClientRecord(message=message, user_agent=user_agent)
+        record = ClientRecord(message=message, user_agent=user_agent, username=username, headers=headers)
         record.save()
 
         return HttpResponse('ok')
