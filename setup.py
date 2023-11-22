c938d8070fb2d8cc35a0d8e769d8c7c0cb305fb6
Maks Motovilov <ismms@yandex.ru>
2023-11-22 09:18:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='verme-logs',
-    version='1.28.38',
+    version='1.28.39',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
