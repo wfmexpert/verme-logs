ff2dcff2b23afbf603f08583b2d241a138126ac7
Aleksandr Kamyanskii <160958027+kammalexx@users.noreply.github.com>
2025-04-17 18:58:37 +0300

    update version

--- setup.py
+++ setup.py
@@ -1,44 +1,44 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any  path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='verme-logs',
-    version='1.28.46',
+    name="verme-logs",
+    version="1.28.47",
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',  # example license
-    description='Verme logs module to catch server processes and client browser errors',
+    license="BSD License",  # example license
+    description="Verme logs module to catch server processes and client browser errors",
     long_description=README,
-    url='https://www.verme.ru/',
-    author='Ilya Tabakov',
-    author_email='i.tabakov@verme.ru',
+    url="https://www.verme.ru/",
+    author="Ilya Tabakov",
+    author_email="i.tabakov@verme.ru",
     install_requires=[
-        'Django>=2.0',
-        'social-auth-app-django',
-        'social-auth-core[saml]',
-        'xlrd',
-        'XlsxWriter==3.2.0',
-        'xlwt',
-        'django-admin-list-filter-dropdown',
-        'openpyxl',
+        "Django>=2.0",
+        "social-auth-app-django",
+        "social-auth-core[saml]",
+        "xlrd",
+        "XlsxWriter==3.2.0",
+        "xlwt",
+        "django-admin-list-filter-dropdown",
+        "openpyxl",
     ],
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 2.0',  # replace "X.Y" as appropriate
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',  # example license
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2.0",  # replace "X.Y" as appropriate
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",  # example license
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
 )
